c75fb6cbaa8a218a82645a6efa1463a43795ff5e
prosacwroc <prosac.wroc@gmail.com>
2018-07-02 20:04:54 +0200

    add Pizza class

---
+++ src/main/java/Pizza.java
@@ -0,0 +1,17 @@
+public class Pizza {
+    private String dough = " ";
+    private String souce = " ";
+    private String topping = " ";
+
+    public void setDough(String dough) {
+        this.dough = dough;
+    }
+
+    public void setSouce(String souce) {
+        this.souce = souce;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
+}
